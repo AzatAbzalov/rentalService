403f802ddec80b55358700dec961a511809213a5
AzatAbzalov <arketech@icloud.com>
2025-05-23 21:46:20 +0300

    feat: implement additional functionality(reviews, favourites, auth)

--- rental-service/server/routes/userRoutes.js
+++ rental-service/server/routes/userRoutes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import upload from '../middleware/upload.js';
-import { registration } from '../controllers/userController.js';
+import { registration, login, logout, checkAuth } from '../controllers/userController.js';
+import { authenticateToken } from "../middleware/authMiddleware.js";
 
 const router = new Router();
 
 router.post('/register', upload.single('avatar'), registration);
+router.post('/login',login);
+router.post('/login', authenticateToken, checkAuth);
+router.delete('/logout', logout);
 
 export default router;
