33b14c69862dd142355ad1bbb73a474a607e854f
AzatAbzalov <arketech@icloud.com>
2025-05-31 03:07:39 +0300

    feat: added routing, mock for favorites and offers

--- rental-service/client/src/components/app/app.tsx
+++ rental-service/client/src/components/app/app.tsx
@@ -1,9 +1,48 @@
 import { MainPage } from "../../pages/main-page/main-page";
-
-function App(){
-    return(
-        <MainPage />
-    )
+import { Login } from "../../pages/login/login";
+import { Favourites } from "../../pages/favourites/favourites";
+import { Offer } from "../../pages/offer/offer";
+import {NotFound} from "../not-found/not-found";
+import { BrowserRouter } from "react-router-dom";
+import { Route } from "react-router-dom";
+import { Routes } from "react-router-dom";
+import { AppRoute, AuthorizationStatus } from "../../const";
+import { JSX } from "react";
+import { PrivateRoute } from "../private-route/private-route";
+import { FullOffer, OffersList } from '../../types/offer';
+  
+type AppMainPageProps = {
+    rentalOffersCount: number;
+    offersList: OffersList[];
+    offers: FullOffer[];
 }
+
+function App({ rentalOffersCount, offers, offersList}: AppMainPageProps): JSX.Element {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    path={AppRoute.Main}
+                    element={<MainPage rentalOffersCount={rentalOffersCount} offersList={ offersList } />} />
+
+                <Route path={AppRoute.Login} element={<Login/>} />
+
+                <Route
+                    path={AppRoute.Favorites}
+                    element={
+                        <PrivateRoute
+                            // authorizationStatus={AuthorizationStatus.NoAuth}
+                            authorizationStatus={AuthorizationStatus.Auth}
+                            >
+                            <Favourites favoritesList={ offersList }   />
+                        </PrivateRoute>} />
+
+                <Route path={ `${AppRoute.Offer}/:id` } element={<Offer offers={offers}/>} />
+                <Route path="*" element={<NotFound/>} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
 
 export { App };
